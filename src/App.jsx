394242183afbdbf5c432fd5a220672f8301c70dc
Leonardo Recalde <leo03recalde@gmail.com>
2024-08-22 16:33:42 -0300

    hasta aqui llego la clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
-
   return (
-    <h1>Hola Mundo</h1>
-  )
+  <main className="container my-4">
+      <h1 className="text-center">Primer Proyecto React</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Vero
+        modi nihil harum perferendis molestiae, soluta quia aliquid, quibusdam
+        ad tenetur suscipit facilis eos doloremque optio totam quae libero, ea
+        dolore!
+      </p>
+    </main>
+  );
 }
 
-export default App
+export default App;
