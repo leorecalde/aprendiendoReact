54e38ca940c516658f230ecea3c2fe6c805e1c49
Leonardo Recalde <leo03recalde@gmail.com>
2024-08-29 15:29:18 -0300

    se agrega funcion para restar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 const Contador = () => {
-    //logica del componente
-    //let numero = 1;
-    //como crear un estado
-const[numero, setNumero] = useState(1);
+  //logica del componente
+  //let numero = 1;
+  //como crear un estado
+  const [numero, setNumero] = useState(1);
 
-    const sumar = () =>{
-        //numero++;
-        setNumero(numero + 1)
-        console.log(numero)
-    }
-    return (
-        <section className="text-center my-5">
-            <h2>Contador</h2>
-            <h4>{numero}</h4>
-            <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
-            <button className="btn btn-danger">-1</button>
-        </section>
-    );
+  const sumar = () => {
+    //numero++;
+    setNumero(numero + 1);
+    console.log(numero);
+    //como tengo una sola linea de codigo, puedo escribirlo dentro de mi 
+    //etiqueta html sin necesidad de crear una funcion, ej en el boton de resta
+  };
+  return (
+    <section className="text-center my-5">
+      <h2>Contador</h2>
+      <h4>{numero}</h4>
+      <button className="btn btn-primary me-2" onClick={sumar}>
+        +1
+      </button>
+      <button className="btn btn-danger" onClick={() => setNumero(numero - 1)}>
+        -1
+      </button>
+    </section>
+  );
 };
 
 export default Contador;
